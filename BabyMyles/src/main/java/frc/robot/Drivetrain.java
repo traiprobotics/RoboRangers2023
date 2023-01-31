e46dc28739b734f9aa973ffd9dc7ddda99afcde3
traiprobotics <35778648+traiprobotics@users.noreply.github.com>
2023-01-31 16:46:59 -0500

    Initial commit

---
+++ BabyMyles/src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,83 @@
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class Drivetrain {
+
+      private static DifferentialDrive driveTrain;
+    
+      private static CANSparkMax frontRightDrive;
+      private static CANSparkMax frontLeftDrive;
+      private static CANSparkMax backRightDrive;
+      private static CANSparkMax backLeftDrive;
+
+      private static float speed;
+      private static final float DEFAULT_SPEED = 0.65f;
+      private static final float SLOW_SPEED = 0.4f;
+
+        private static boolean toggleSlowSpeed = true;
+
+//   static SlewRateLimiter filter = new SlewRateLimiter(0.5);
+//   static SlewRateLimiter filter2 = new SlewRateLimiter(0.5);
+
+public Drivetrain() {
+
+  frontRightDrive = new CANSparkMax(3, MotorType.kBrushless);
+  frontLeftDrive = new CANSparkMax(4, MotorType.kBrushless);
+  backRightDrive = new CANSparkMax(1, MotorType.kBrushless);
+  backLeftDrive = new CANSparkMax(2, MotorType.kBrushless);
+
+  frontRightDrive.setInverted(false);
+  frontLeftDrive.setInverted(false);
+  backRightDrive.setInverted(false);
+  backLeftDrive.setInverted(false);
+
+  backRightDrive.follow(frontRightDrive);
+  backLeftDrive.follow(frontLeftDrive);
+
+  driveTrain = new DifferentialDrive(frontLeftDrive, frontRightDrive);
+
+  speed = DEFAULT_SPEED;
+
+}
+
+public static void driveArcade(float pRotation, float pSpeed) {
+    driveTrain.arcadeDrive(pSpeed, pRotation);
+  }
+  
+  public static void driveTank(float pLeftSpeed, float pRightSpeed) {
+ //  driveTrain.tankDrive(filter.calculate(pLeftSpeed), filter2.calculate(pRightSpeed));
+   driveTrain.tankDrive(pLeftSpeed, pRightSpeed);
+  }
+
+  public static void tankDriveWithJoystick() {
+      float right = (float) (IO.driveJoystick.getRawAxis(IO.RY_STICK_AXIS)* speed);
+      float left = (float) (IO.driveJoystick.getRawAxis(IO.LY_STICK_AXIS)* -speed);
+  
+      driveTank(left, right);
+  }
+
+  public static void toggleSlowSpeed() {
+    if(toggleSlowSpeed) {
+      toggleSlowSpeed = false;
+  
+      if(speed == DEFAULT_SPEED) {
+        speed = SLOW_SPEED;
+      } else {
+        speed = DEFAULT_SPEED;
+      }
+    }
+  }
+
+
+
+
+
+
+}
+
+
+
+
---
+++ BabyMyles/src/main/java/frc/robot/IO.java
@@ -0,0 +1,98 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class IO {
+
+    public static final int DRIVE_JOYSTICK_PORT = 0;
+    public static final int CONTROL_JOYSTICK_PORT = 1;
+
+    public static final int LX_STICK_AXIS = 0;
+    public static final int LY_STICK_AXIS = 1;
+    public static final int RX_STICK_AXIS = 2;
+    public static final int RY_STICK_AXIS = 3;
+
+    public static final int L_TRIGGER_AXIS = 2;
+    public static final int R_TRIGGER_AXIS = 3;
+
+    public static final int PAD = 16;
+
+    public static final int A_BUTTON = 1;
+    public static final int B_BUTTON = 2;
+    public static final int X_BUTTON = 3;
+    public static final int Y_BUTTON = 4;
+    public static final int LB_BUTTON = 5;
+    public static final int RB_BUTTON = 6;
+    public static final int BACK_BUTTON = 7;
+    public static final int START_BUTTON = 8;
+    public static final int L_STICK_BUTTON = 9;
+    public static final int R_STICK_BUTTON = 10;
+    public static final int L_TRIGGER_BUTTON = 11;
+    public static final int R_TRIGGER_BUTTON = 12;
+
+    public static Joystick driveJoystick;
+    public static Joystick shootJoystick;
+
+    public IO() {
+        driveJoystick = new Joystick(DRIVE_JOYSTICK_PORT);
+        shootJoystick = new Joystick(CONTROL_JOYSTICK_PORT);
+    }
+
+    public static int buttonPressed(Joystick pJoystick) {
+        
+        if(pJoystick.getRawButton(A_BUTTON)) {
+            return A_BUTTON;
+        } else if(pJoystick.getRawButton(B_BUTTON)) {
+            return B_BUTTON;
+        } else if(pJoystick.getRawButton(X_BUTTON)) {
+            return X_BUTTON;
+        } else if(pJoystick.getRawButton(Y_BUTTON)) {
+            return Y_BUTTON;
+        } else if (pJoystick.getRawButton(LB_BUTTON)) {
+            return LB_BUTTON;
+        } else if (pJoystick.getRawButton(RB_BUTTON)) {
+            return RB_BUTTON;
+        } else if (pJoystick.getRawButton(BACK_BUTTON)) {
+            return BACK_BUTTON;
+        } else if (pJoystick.getRawButton(START_BUTTON)) {
+            return START_BUTTON;
+        } else if (pJoystick.getRawButton(L_STICK_BUTTON)) {
+            return L_STICK_BUTTON;
+        } else if (pJoystick.getRawButton(R_STICK_BUTTON)) {
+        return R_STICK_BUTTON;
+        } else if (pJoystick.getRawAxis(L_TRIGGER_AXIS) > 0.1f) {
+        return L_TRIGGER_BUTTON;
+        } else if(pJoystick.getRawAxis(R_TRIGGER_AXIS) > 0.1f) {
+        return R_TRIGGER_BUTTON;
+        } else if(pJoystick.getPOV() > -1){
+            return PAD;
+        } else {
+            return 0;
+        } 
+    } 
+
+    public static void driveButtonsPressed() {              //DRIVE CONTROLLER
+        switch (buttonPressed(IO.driveJoystick)) {
+            case A_BUTTON:
+                break;
+            case B_BUTTON:
+                break;
+            case X_BUTTON:  
+                break;
+            case Y_BUTTON:
+                break;
+            case LB_BUTTON:
+                break;
+            case RB_BUTTON:
+                break;
+            case BACK_BUTTON:
+                // changeLayoutBackwards();
+                break;
+            case START_BUTTON:
+                // changeLayoutForwards();
+                break;
+            default:
+            break;
+        }
+    }
+}
